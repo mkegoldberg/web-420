7ca8c8643ab8b0e962e3c523e75827a353ee5593
Mike Goldberg <mkegoldberg@yahoo.com>
2020-05-01 23:07:37 -0700

    adding api-gateway

---
+++ goldberg-header.js
@@ -0,0 +1,20 @@
+/*
+============================================
+; Title:  header.js
+; Author: Mike Goldberg
+; Date:   30 April 2020
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/**
+ * Params: firstName, lastName, assignment
+ * Response: output
+ * Description: Returns a well-formatted string header
+ */
+exports.display = function (firstName, lastName, assignment) {
+  let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' +
+    new Date().toLocaleDateString()
+
+  return output
+}
