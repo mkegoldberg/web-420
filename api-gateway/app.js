efba6fc9f22df5642aeb0acde104cb4a5d6d13c7
Mike Goldberg <mkegoldberg@yahoo.com>
2020-05-08 07:50:16 -0700

    fixing app.js

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -42,7 +42,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('api', apiCatalog);
+app.use('/api', apiCatalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
