2ae41b2e79f2d4cef9b190c191e0e5a2a3be8775
Mike Goldberg <mkegoldberg@yahoo.com>
2020-05-23 09:39:38 -0700

    adding code attribution on index.js

--- api-gateway/routes/index.js
+++ api-gateway/routes/index.js
@@ -1,3 +1,12 @@
+/*
+============================================
+; Title:  API Gateway
+; Author: Mike Goldberg
+; Date:   May 4 2020
+; Description: REST
+;===========================================
+*/
+
 var express = require('express');
 var router = express.Router();
 
